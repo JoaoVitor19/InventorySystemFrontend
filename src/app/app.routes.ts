1ec9654c538bf3dd810605eab9856c4534f19d17
João Vitor <jaovitordev@gmail.com>
2024-06-18 00:57:15 -0300

    feat: primeiro commit real da branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryPasswordComponent } from './pages/recovery-password/recovery-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'recover-password', component: RecoveryPasswordComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Validators,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
+
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private authService: AuthService) { }
 
+  form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  showPassword: boolean = false;
+
+  pathImageLogin = "assets/images/food_image_1.jpg";
+  pathLogoLogin = "assets/logos/logo-caipirao.png";
+  ngOnInit() {
+
+  }
+
+  setPasswordVisilibity() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+    let form = this.form.getRawValue();
+    this.authService.login(form.email, form.password);
+    this.snackBar.open("Seja bem vindo", "Fechar", { duration: 750 });
+  }
 }
